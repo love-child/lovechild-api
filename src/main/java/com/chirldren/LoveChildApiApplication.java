c6832eb3707042369b7fd849a826552f2fe808f8
SunBo1994 <sunboEIP@163.com>
2020-08-06 15:25:03 +0800

    初始化 项目

---
+++ src/main/java/com/chirldren/LoveChildApiApplication.java
@@ -0,0 +1,15 @@
+package com.chirldren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * author 孙博
+ * date 2020/8/6 15:19
+ */
+@SpringBootApplication
+public class LoveChildApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoveChildApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/children/LoveChildApiApplicationTests.java
@@ -0,0 +1,18 @@
+package com.children;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * author 孙博
+ * date 2020/8/6 15:21
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoveChildApiApplicationTests {
+    @Test
+    public void contextLoads() {
+    }
+}
